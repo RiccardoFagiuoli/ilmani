9503071ee5840f3491e2c0de48dc48dbf37e7812
Riccardo <riccardo.fagiuoli@edu.unifi.it>
2026-04-25 18:34:30 +0200

    Reformat code

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,22 +1,22 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import {Footer} from './components/core/footer/footer';
-import {Sidebar} from './components/core/sidebar/sidebar';
-import {Topbar} from './components/core/topbar/topbar';
-import {Header} from './components/core/header/header';
-import {Navmenu} from './components/core/navmenu/navmenu';
-import {Articles} from './components/layout/articles/articles';
-import {Hero} from './components/layout/hero/hero';
-import {Cards} from './components/layout/cards/cards';
-import {Subscriptionbox} from './components/features/subscriptionbox/subscriptionbox';
-import {Copyright} from './components/core/copyright/copyright';
+/*import { RouterOutlet } from '@angular/router';*/
+import { Footer } from './components/core/footer/footer';
+import { Sidebar } from './components/core/sidebar/sidebar';
+import { Topbar } from './components/core/topbar/topbar';
+import { Header } from './components/core/header/header';
+import { Navmenu } from './components/core/navmenu/navmenu';
+import { Articles } from './components/layout/articles/articles';
+import { Hero } from './components/layout/hero/hero';
+import { Cards } from './components/layout/cards/cards';
+import { Subscriptionbox } from './components/features/subscriptionbox/subscriptionbox';
+import { Copyright } from './components/core/copyright/copyright';
 import { Ad } from './components/features/ad/ad';
 import { Alias } from './components/layout/alias/alias';
 
 @Component({
   selector: 'app-root',
   imports: [
-    RouterOutlet,
+    /*RouterOutlet,*/
     Footer,
     Sidebar,
     Topbar,
